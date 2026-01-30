4b23733602bfa8b9a3b3a8216473bfcf9872acd2
Dikara Derasetiana <dikaraderandia.jtk10@gmail.com>
2026-01-30 22:56:49 +0700

    Feature login (#3)
    * feat(auth-service): feature login, refresh token, logout completed
    
    * feat(auth-service): add param roles when generate accesstoken
    
    * feat(auth-service): integrated with eureka
    
    * feat(auth-service): add communication with user -service using feign eureka
    
    * feat(auth-service): fixing userClient feign eureka body response adjustment with user service
    
    * feat(auth-service): add Dockerfile
    
    * feat(auth-service): implement swagger and adjustement for feign service client
    
    ---------
    
    Co-authored-by: Dikara Derandia <dikara.derandia@dwidasa.com>

--- src/main/java/com/dikara/auth/config/SecurityConfig.java
+++ src/main/java/com/dikara/auth/config/SecurityConfig.java
@@ -29,7 +29,9 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**","/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
